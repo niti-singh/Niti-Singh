c525751b6b79e9c1ae58699994ad65e322d3b4ef
Niti Singh Goutam <124861220+niti-singh@users.noreply.github.com>
2024-04-05 09:49:19 +0530

    added JavaScript.js

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+ var typed=new Typed(".input",{
+    strings:["Frontend Developer"],
+    typedSpeed:70,
+    backSpeed:55,
+    loop:true
+})
+
+
+//-------------Sideb baar-----------
+
+var sidemenu = document.getElementById("side-menu");
+function openmenu(){
+sidemenu.style.right = "0";
+}
+function closemenu(){
+sidemenu.style.right = "-200px";
+}
+
+
